30254eaaa08955376a7b7ef6ac8e2e71ee837c0e
JeremyMet <metairie.jeremy@gmail.com>
2018-11-15 22:11:36 +0100

    casting engine (WIP)

--- sketch.py
+++ sketch.py
@@ -15,20 +15,24 @@ pygame.key.set_repeat(400, 30)
 
 class pyCast(object):
 
-    def __init__(self, surface, level_array, tile_dim):
+    def __init__(self, surface, level_array):
         self.surface = surface ;
         self.level_array = level_array ;
         self.go = True ;
-        self.tile_dim = tile_dim ;
         self.x_p = 0 ;
         self.y_p = 0 ;
         self.angle = 0 ;
         # "constants part"
         self.LEVEL_HEIGHT = len(level_array) ;
         self.LEVEL_WIDTH = len(level_array[0]) ;
-        self.FOV = 120 ;
         self.WALK_SPEED = 2 ;
         self.ROT_SPEED = 2 ;
+        # engine constant part
+        self.SCREEN_HEIGHT = 480 ;
+        self.SCREEN_WIDTH = 640 ;
+        self.FOV = 120 ;
+        self.tile_dim = 32;
+
 
 
     def ctrl(self):
@@ -52,15 +56,14 @@ class pyCast(object):
                 if dir:
                     tmp_x_p = self.x_p+dir*self.WALK_SPEED*math.cos(math.radians(self.angle)) ;
                     tmp_y_p = self.y_p-dir*self.WALK_SPEED*math.sin(math.radians(self.angle)) ;
-                    scaled_tmp_x_p = int(tmp_x_p) // self.tile_dim[1] ;
-                    scaled_tmp_y_p = int(tmp_y_p) // self.tile_dim[1] ;
+                    scaled_tmp_x_p = int(tmp_x_p) // self.tile_dim ;
+                    scaled_tmp_y_p = int(tmp_y_p) // self.tile_dim ;
                     if scaled_tmp_x_p >= 0 and scaled_tmp_x_p < self.LEVEL_WIDTH:
-                        if not(self.level_array[int(self.y_p) // self.tile_dim[1]][scaled_tmp_x_p]):
+                        if not(self.level_array[int(self.y_p) // self.tile_dim][scaled_tmp_x_p]):
                             self.x_p = tmp_x_p ;
                     if scaled_tmp_y_p >= 0 and scaled_tmp_y_p < self.LEVEL_HEIGHT:
-                        if not(self.level_array[scaled_tmp_y_p][int(self.x_p) // self.tile_dim[1]]):
+                        if not(self.level_array[scaled_tmp_y_p][int(self.x_p) // self.tile_dim]):
                             self.y_p = tmp_y_p ;
-
 
     def set_pos(self, x, y):
         self.x_p = x ;
@@ -68,15 +71,30 @@ class pyCast(object):
 
     def draw_hero(self):
         # pygame.draw.rect(self.surface, (128, 64, 32), (self.x_p, self.y_p, 15, 15)) ;
-        angleFov_0 = math.radians((self.angle+self.FOV//2)%360) ;
-        angleFov_1 = math.radians((self.angle-self.FOV//2)%360) ;
-        angle = math.radians(self.angle)
-        pygame.draw.line(self.surface, (0, 128, 0), (self.x_p, self.y_p), (self.x_p+100*math.cos(angleFov_0), \
-        self.y_p-100*math.sin(angleFov_0)), 2) ;
-        pygame.draw.line(self.surface, (0, 128, 0), (self.x_p, self.y_p), (self.x_p+100*math.cos(angleFov_1), \
-        self.y_p-100*math.sin(angleFov_1)), 2) ;
-        pygame.draw.line(self.surface, (128, 0, 0), (self.x_p, self.y_p), (self.x_p+100*math.cos(angle), \
-        self.y_p-100*math.sin(angle)), 2) ;
+        min_bound = self.angle-(self.FOV>>1) ;
+        max_bound = self.angle+(self.FOV>>1) ;
+        #max_bound = min_bound ; # TO DO
+        for angle in range(min_bound, max_bound):
+
+            # Cast the "Ray" tile per tile
+            angle = math.radians(angle%360)
+            x_slope = math.cos(angle) ;
+            y_slope = math.tan(angle) ; # TODO tan : INF
+            if x_slope > 0:
+                x_step = self.tile_dim-(self.x_p % self.tile_dim) ;
+                x_cast = self.x_p+x_step ;
+            else:
+                x_step = (self.x_p % self.tile_dim) ;
+                x_cast = self.x_p-x_step ;
+            y_cast = self.y_p + y_slope * x_step;
+
+            while((x_cast >= 0 and x_cast < self.LEVEL_WIDTH) \
+            and (y_cast >= 0 and y_cast < self.LEVEL_HEIGHT) \
+            and not(self.level_array[y_cast][x_cast])):
+                x_cast += self.tile_dim  ;
+                y_cast += y_slope ;
+            pygame.draw.line(self.surface, (0, 128, 0), (self.x_p, self.y_p), (x_cast, y_cast), 2) ;
+
 
 
 
@@ -93,15 +111,15 @@ class pyCast(object):
         for line in level_array:
             for val in line:
                 if val:
-                    pygame.draw.rect(self.surface, color_white, (x_pos, y_pos, self.tile_dim[0], self.tile_dim[1]), 0) ;
-                x_pos += self.tile_dim[0] ;
-            y_pos += self.tile_dim[1] ;
+                    pygame.draw.rect(self.surface, color_white, (x_pos, y_pos, self.tile_dim, self.tile_dim), 0) ;
+                x_pos += self.tile_dim ;
+            y_pos += self.tile_dim ;
             x_pos = 0 ;
 
 
 level_array = [[1,1,1,1,1], [1,0,0,0,1], [1,0,1,0,1], [1, 0, 0, 0, 1], [1, 1, 1,1, 1]] ;
 
-pyCastInst = pyCast(fenetre, level_array, (32, 32)) ;
+pyCastInst = pyCast(fenetre, level_array) ;
 pygame.display.flip() ;
 pyCastInst.set_pos(32 ,32) ;
 
